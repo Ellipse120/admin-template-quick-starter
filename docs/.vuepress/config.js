5e7c91d2414125ea09e7643e6ec6bd6daa9b4641
ellipse120 <ellipse120@gmail.com>
2022-03-11 16:44:40 +0800

    docs: add favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,32 +1,36 @@
 module.exports = {
-  title: 'Admin Template Quick Starter',
-  description: 'A simple and beautiful vuepress blog',
+  title: "Admin Template Quick Starter",
+  description: "A simple and beautiful vuepress blog",
   port: 8100,
-  base: '/docs/',
+  base: "/docs/",
   markdown: {
     lineNumbers: true
   },
+  head: [["link", { rel: "icon", type: "icon", href: "./favicon.ico" }]],
   themeConfig: {
-    logo: '/logo.png',
+    logo: "/logo.png",
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'Github', link: 'https://github.com/Ellipse120/admin-template-quick-starter' }
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      {
+        text: "Github",
+        link: "https://github.com/Ellipse120/admin-template-quick-starter"
+      }
     ],
     displayAllHeaders: true,
     smoothScroll: true,
     sidebar: [
-      '/',
-      '/development',
-      '/architecture',
-      '/tech',
-      '/routing',
-      '/state',
-      '/tests',
-      '/linting',
-      '/editors',
-      '/production',
-      '/troubleshooting',
+      "/",
+      "/development",
+      "/architecture",
+      "/tech",
+      "/routing",
+      "/state",
+      "/tests",
+      "/linting",
+      "/editors",
+      "/production",
+      "/troubleshooting"
     ]
   }
-}
+};
