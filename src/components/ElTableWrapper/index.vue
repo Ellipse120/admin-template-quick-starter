7987887e632e2886865a417eef9974b75ea8f633
ellipse120 <ellipse120@gmail.com>
2023-02-21 11:05:13 +0800

    feat(ElTableWrapper): add `fixed` feature like `Excel` frozen column

--- src/components/ElTableWrapper/index.vue
+++ src/components/ElTableWrapper/index.vue
@@ -1,9 +1,12 @@
 <template>
   <el-table
+    ref="elTableRef"
     :height="height"
     :border="border"
     :stripe="stripe"
+    :header-cell-class-name="setFixedColumnsClass"
     v-bind="$attrs"
+    @header-click="toggleFixedColumns"
     v-on="$listeners"
   >
     <slot />
@@ -29,6 +32,42 @@ export default {
     height: {
       type: [String, Number],
       default: '100%'
+    }
+  },
+  data () {
+    return {
+      isFixed: false, // toggle fixed state
+      fixedColumnIndex: null // column index of target column
+    }
+  },
+  methods: {
+    setFixedColumnsClass ({ columnIndex }) {
+      if (this.isFixed && (columnIndex <= this.fixedColumnIndex)) {
+        return 'cursor-pointer !bg-red-200 !text-white'
+      }
+
+      return 'cursor-pointer'
+    },
+
+    toggleFixedColumns (column) {
+      this.isFixed = !this.isFixed
+      const columnValue = column.label
+      const elTableRef = this.$refs['elTableRef']
+
+      this.fixedColumnIndex = elTableRef.store.states._columns.findIndex(c => {
+        const cValue = c.label
+
+        return cValue === columnValue
+      })
+
+      elTableRef.store.states._columns.forEach((c, index) => {
+        if (index <= this.fixedColumnIndex) {
+          c.fixed = this.fixed
+        }
+      })
+
+      elTableRef.store.updateColumns()
+      elTableRef.doLayout()
     }
   }
 }
